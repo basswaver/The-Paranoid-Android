65311501a55f00e8cfb05b9a8c50a8ec224afb1c
Zero <dakoolstwunn@gmail.com>
2019-04-24 10:16:49 -0400

    oops wrong links

--- marvin.py
+++ marvin.py
@@ -93,7 +93,7 @@ def get_quote():
 
 if __name__ == "__main__":
 	while True:
-		sub = "+".join(('ShittySCP', 'InteractiveFoundation', 'SCP_Game', 'sandboxtest', 'SCP682', 'DankMemesFromSite19', 'WholesomeSite19Memes', 'okbuddyredacted'))
+		sub = "+".join(("UnexpectedSCP", "elephand", "ExplainLikeImSCP", "SCPokemon", "SCPorn", "thechurchofpeanut", "scpfuel", "shittySCP"))
 		sleep(2)
 		try:
 			for comment in r.subreddit(sub).stream.comments():
